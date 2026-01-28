8675a147c06e31e8cd3abce2b95b87c923a83264
Devansh-Bansal-AI <devansh.24bai10389@vitbhopal.ac.in>
2026-01-28 19:02:23 +0530

    Updated tests.py

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .services import DataService
 
-# Create your tests here.
+class ServiceTests(TestCase):
+    def test_calculations(self):
+        # Create a dummy CSV or dataframe logic here
+        pass
